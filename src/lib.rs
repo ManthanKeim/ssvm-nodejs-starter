54ea49c08b86efb768ae05144b1e03c5d835dddb
manthankeim <manthankeim7@gmail.com>
2020-08-13 22:23:36 +0530

    Calculate Distance Between Two Points

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+use std::io;
 use wasm_bindgen::prelude::*;
+use std::num::sqrt;
+use std::num::powi;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn dbtp(x1:f64,y1:f64,x2:f64,y2:f64)->f64{
+    return ((((x2-x1).powi(2)) + ((y2-y1).powi(2))).sqrt())
 }
